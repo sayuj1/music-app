5d702689361405984adb4bb9aeeb09ca2eae4d23
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-05-11 23:15:35 +0530

    play image added

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from tkinter import *
 
 root = Tk()
 root.geometry('500x500')
+
 root.title('Melody')
 root.iconbitmap(r'melody.ico')
+text = Label(root, text='Lets make some noise')
+text.pack()
+photo = PhotoImage(file='play.png')
+labelphoto = Label(root, image = photo)
+labelphoto.pack()
+
 root.mainloop()
