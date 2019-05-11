5327052cb8309bfcf30aa4fedf9e31d0c8a556e7
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-05-11 23:55:17 +0530

    volume control added

--- main.py
+++ main.py
@@ -12,13 +12,30 @@ root.iconbitmap(r'melody.ico')
 text = Label(root, text='Lets make some noise')
 text.pack()
 
-photo = PhotoImage(file='play.png')
+#set volume
+def set_vol(val):
+    volume = int(val)/100
+    mixer.music.set_volume(volume)
 
+#play music
 def play_music():
     mixer.music.load('ChillingMusic.wav')
     mixer.music.play()
 
-playBtn = Button(root, image = photo, command=play_music)
+#stop music
+def stop_music():
+    mixer.music.stop()
+
+play_photo = PhotoImage(file='play.png')
+playBtn = Button(root, image = play_photo, command=play_music)
 playBtn.pack()
+
+stop_photo = PhotoImage(file='stop.png')
+stopBtn = Button(root, image = stop_photo, command=stop_music)
+stopBtn.pack()
+
+scale = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
+scale.set(20)
+scale.pack()
 
 root.mainloop()
