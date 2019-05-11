a9ae4200811686f4852120188a5bf1d41497ec45
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-05-11 23:33:11 +0530

    mixer installed | music added

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from tkinter import *
+from pygame import mixer
 
 root = Tk()
+
+mixer.init() #initializing the mixer
+
 root.geometry('500x500')
 
 root.title('Melody')
 root.iconbitmap(r'melody.ico')
 text = Label(root, text='Lets make some noise')
 text.pack()
+
 photo = PhotoImage(file='play.png')
-labelphoto = Label(root, image = photo)
-labelphoto.pack()
+
+def play_music():
+    mixer.music.load('ChillingMusic.wav')
+    mixer.music.play()
+
+playBtn = Button(root, image = photo, command=play_music)
+playBtn.pack()
 
 root.mainloop()
