11028e2ce91c86017930ee4035f1f8593ccb36b7
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-05-12 08:41:25 +0530

    open file dialog added

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 from tkinter import *
-import tkinter.messagebox
+import tkinter.messagebox as mb
+import tkinter.filedialog as fs
 from pygame import mixer
 
 root = Tk()
 
-#create the menubar
+# create the menubar
 menubar = Menu(root)
 root.config(menu=menubar)
 
-#create the submenu
-subMenu = Menu(menubar, tearoff=0) #tearoff --> remove the dashed line
+# create the submenu
+subMenu = Menu(menubar, tearoff=0)  # tearoff --> remove the dashed line
+
+
+def open_file():
+    global filename
+    filename = fs.askopenfilename()
+    # print(filename)
+
+
 menubar.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label = "Open")
+subMenu.add_command(label="Open", command=open_file)
 subMenu.add_separator()
-subMenu.add_command(label = "Exit", command = root.destroy)
+subMenu.add_command(label="Exit", command=root.destroy)
+
 
 def about_us():
-    tkinter.messagebox.showinfo('About Melody','The is the music listening app created by Sayuj')
+    mb.showinfo('About Melody',
+                'The is the music listening app created by Sayuj')
 
-subMenu = Menu(menubar, tearoff=0) #tearoff --> remove the dashed line
+
+subMenu = Menu(menubar, tearoff=0)  # tearoff --> remove the dashed line
 menubar.add_cascade(label="Help", menu=subMenu)
-subMenu.add_command(label = "About Us", command = about_us)
+subMenu.add_command(label="About Us", command=about_us)
 
 # root.resizable(False, False)
-mixer.init() #initializing the mixer
+mixer.init()  # initializing the mixer
 
 root.geometry('500x500')
 
@@ -32,34 +44,45 @@ root.iconbitmap(r'melody.ico')
 text = Label(root, text='Lets make some noise')
 text.pack()
 
-#set volume
+# set volume
+
+
 def set_vol(val):
     volume = int(val)/100
     mixer.music.set_volume(volume)
 
-#play music
-def play_music():
-    mixer.music.load('ChillingMusic.wav')
-    mixer.music.play()
+# play music
 
-#stop music
+
+def play_music():
+    try:
+        mixer.music.load(filename)
+        mixer.music.play()
+
+    except:
+        mb.showerror('No File', 'No File Choosen')
+
+# stop music
+
+
 def stop_music():
     mixer.music.stop()
 
-#play button
+
+# play button
 play_photo = PhotoImage(file='play.png')
-playBtn = Button(root, image = play_photo, command=play_music)
+playBtn = Button(root, image=play_photo, command=play_music)
 playBtn.pack()
 
-#stop button
+# stop button
 stop_photo = PhotoImage(file='stop.png')
-stopBtn = Button(root, image = stop_photo, command=stop_music)
+stopBtn = Button(root, image=stop_photo, command=stop_music)
 stopBtn.pack()
 
-#volume Control
+# volume Control
 scale = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.set(20)
-mixer.music.set_volume(0.2) #setting default volume 
+mixer.music.set_volume(0.2)  # setting default volume
 scale.pack()
 
 root.mainloop()
