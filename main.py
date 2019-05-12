d97f882410a821009f6e7b69d6ff51e2d12d1347
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-05-12 08:28:55 +0530

    menu | submenu added

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from tkinter import *
+import tkinter.messagebox
 from pygame import mixer
 
 root = Tk()
 
+#create the menubar
+menubar = Menu(root)
+root.config(menu=menubar)
+
+#create the submenu
+subMenu = Menu(menubar, tearoff=0) #tearoff --> remove the dashed line
+menubar.add_cascade(label="File", menu=subMenu)
+subMenu.add_command(label = "Open")
+subMenu.add_separator()
+subMenu.add_command(label = "Exit", command = root.destroy)
+
+def about_us():
+    tkinter.messagebox.showinfo('About Melody','The is the music listening app created by Sayuj')
+
+subMenu = Menu(menubar, tearoff=0) #tearoff --> remove the dashed line
+menubar.add_cascade(label="Help", menu=subMenu)
+subMenu.add_command(label = "About Us", command = about_us)
+
+# root.resizable(False, False)
 mixer.init() #initializing the mixer
 
 root.geometry('500x500')
@@ -26,16 +46,20 @@ def play_music():
 def stop_music():
     mixer.music.stop()
 
+#play button
 play_photo = PhotoImage(file='play.png')
 playBtn = Button(root, image = play_photo, command=play_music)
 playBtn.pack()
 
+#stop button
 stop_photo = PhotoImage(file='stop.png')
 stopBtn = Button(root, image = stop_photo, command=stop_music)
 stopBtn.pack()
 
+#volume Control
 scale = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.set(20)
+mixer.music.set_volume(0.2) #setting default volume 
 scale.pack()
 
 root.mainloop()
